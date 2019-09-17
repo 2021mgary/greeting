e6cdc6ec2fdcba69f54697e39bbba326655718df
2021mgary <2021mgary@dwight.edu>
2019-09-17 13:33:50 +0000

    second

---
+++ greeting.py
@@ -0,0 +1,15 @@
+name1 ="michaela"
+name2 = "mkay kay"
+age1 = "16"
+age2 = "16"
+
+print(name1)
+print(name2)
+print(len(name1))
+print(len(name2))
+print("hello " + name1)
+print("hello " + name2)
+print(age1)
+print(age2)
+print(name1 + age1)
+print(name2 + age2)
